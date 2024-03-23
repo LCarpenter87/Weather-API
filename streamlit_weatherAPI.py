58b3f23b7e0e293425e8b99fb4ed88c3954f537e
Akshaya <78446081+Akshayap05@users.noreply.github.com>
2024-03-23 21:06:47 +0000

    Update streamlit_weatherAPI.py

--- streamlit_weatherAPI.py
+++ streamlit_weatherAPI.py
@@ -2,9 +2,20 @@ import streamlit as st
 import psycopg2
 import pandas as pd
 
+@st.cache_resource
+def init_connection():
+    return psycopg2.connect(**st.secrets["pagilla"])
 
-# Initialize connection.
-conn = st.connection("pagilla_db", type="sql")
+conn = init_connection()
 
-# Perform query.
-df = conn.query('SELECT * FROM weather;', ttl="10m")
+@st.cache_data(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from weather limit 20")
+
+data=pd.DataFrame(rows)
+data.columns=['location', 'date', 'current_time', 'time_updated', 'temperature', 'condition', 'wind_speed_mph', 'wind_direction', 'humidity', 'cloud', 'uv_index', 'co', 'no2', 'o3']
+st.table(data)
