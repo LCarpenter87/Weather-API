674236864a3f74b9cd8dda1825177c4b23bb132e
Akshaya <78446081+Akshayap05@users.noreply.github.com>
2024-03-23 21:23:59 +0000

    Add files via upload

---
+++ streamlit_weatherAPI.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import psycopg2
+import pandas as pd
+
+@st.cache_resource
+def init_connection():
+    return psycopg2.connect(**st.secrets["pagilla"])
+
+conn = init_connection()
+
+st.title('Welcome to our Weather App')
+
+@st.cache_data(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from weather limit 20")
+
+data=pd.DataFrame(rows)
+data.columns=['location', 'country', 'date', 'current_time', 'time_updated', 'temperature', 'condition', 'wind_speed_mph', 'wind_direction', 'humidity', 'cloud', 'uv_index', 'co', 'no2', 'o3']
+st.table(data)
+
