caaf323c5512750209dcf92d31dbf7a089380269
Akshaya <Akshayap0504@gmail.com>
2024-03-24 22:52:29 +0000

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -67,11 +67,11 @@ with right_col:
 load_dotenv()
 
 # Get database credentials 
-DB_NAME = st.secrets("DB_NAME")
-DB_USER = st.secrets("DB_USER")
-DB_PASSWORD = st.secrets("DB_PASSWORD")
-DB_HOST = st.secrets("DB_HOST")
-DB_PORT = st.secrets("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 
 # set up db connection:
 
