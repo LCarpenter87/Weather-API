c2e38aa2c63b00e68a8e3694f36f4f64e0074f58
Akshaya <Akshayap0504@gmail.com>
2024-03-24 16:33:55 +0000

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import requests
+import numpy as np
+import sqlalchemy
+#import psycopg2
+import pandas as pd
+
+st.title('Welcome to our Weather App')
+
+
+def get_details(cities):
+    try:
+        url = f'http://api.weatherapi.com/v1/current.json?key=787a74aa607147a19bb222554241903&q={cities}&aqi=yes'
+        response = requests.get(url)
+        weather = response.json()
+        temperature = weather['current']['temp_c']
+        condition = weather['current']['condition']['text']
+        humidity = weather['current']['humidity']
+        Cloud_cover= weather['current']['cloud']
+        UV_index= weather['current']['uv']
+        CO = weather['current']['air_quality']['co']
+        NO2= weather['current']['air_quality']['no2']
+        Ozone= weather['current']['air_quality']['o3']
+        return temperature, condition, humidity, Cloud_cover, UV_index, CO, NO2, Ozone
+        
+    except:
+        return 'Error', np.NAN, np.NAN, np.NAN, np.NAN, np.NAN, np.NAN, np.NAN, np.NAN
+
+cities = ['London', 'Manchester', 'Birmingham', 'Glasgow', 'Leeds', 'Liverpool', 'Sheffield', 'Bristol', 'Edinburgh', 'Leicester',  'York', 'Cardiff', 'Brighton', 'Coventry', 'Bath']
+selected_city = st.sidebar.selectbox('Select a city', cities)
+
+weather_data = temperature, condition, humidity, Cloud_cover, UV_index, CO, NO2, Ozone = get_details(selected_city)
+
+if weather_data:
+    st.title(f"{selected_city}")
+    st.write(f"Temperature: {temperature}°C")
+    st.write(f"Condition: {condition}")
+    st.write(f"Humidity: {humidity}%")
+    st.write(f"Cloud Cover: {Cloud_cover}%")
+    st.write(f"UV Index: {UV_index}")
+    st.write(f"CO: {CO}")
+    st.write(f"NO2: {NO2}")
+    st.write(F"Ozone (O3): {Ozone}")
+else:
+    st.error("Failed to fetch weather data. Please try again later.")
