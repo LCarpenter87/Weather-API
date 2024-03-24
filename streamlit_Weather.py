e0259ec151006ea977814199c67a65d0cf0b6355
Akshaya <Akshayap0504@gmail.com>
2024-03-24 16:07:10 +0000

    Update streamlit_Weather.py

--- streamlit_Weather.py
+++ streamlit_Weather.py
@@ -1,15 +1,44 @@
 import streamlit as st
-#import requests
+import requests
+import numpy as np
 #import psycopg2
 import pandas as pd
 
 st.title('Welcome to our Weather App')
 
+
+def get_details(cities):
+    try:
+        url = f'http://api.weatherapi.com/v1/current.json?key=787a74aa607147a19bb222554241903&q={cities}&aqi=yes'
+        response = requests.get(url)
+        weather = response.json()
+        return weather
+    except:
+        return 'Error', np.NAN, np.NAN, np.NAN
+    
+# List of cities
+cities = ['London', 'Manchester', 'Birmingham', 'Glasgow', 'Leeds', 'Liverpool', 'Sheffield', 'Bristol', 'Edinburgh', 'Leicester',  'York', 'Cardiff', 'Brighton', 'Coventry', 'Bath']
+selected_city = st.sidebar.selectbox('Select a city', cities)
+
+weather_data = get_details(selected_city)
+
+if weather_data:
+    st.title(f"Weather Information for {selected_city}")
+    st.write(f"Temperature: {weather_data['current']['temp_c']}°C")
+    st.write(f"Condition: {weather_data['current']['condition']['text']}")
+    st.write(f"Humidity: {weather_data['current']['humidity']}%")
+    st.write(f"Wind Speed: {weather_data['current']['wind_kph']} km/h")
+    st.write(f"Cloud Cover: {weather_data['current']['cloud']}%")
+    st.write(f"UV Index: {weather_data['current']['uv']}")
+else:
+    st.error("Failed to fetch weather data. Please try again later.")
+
+
 # Initialize connection.
-conn = st.connection("pagila", type="sql")
+#conn = st.connection("pagila", type="sql")
 
 # Perform query.
-df = conn.query('SELECT * FROM weather limit 15')
-st.write(df)
+#df = conn.query('SELECT * FROM weather limit 15')
+#st.write(df)
 
 
