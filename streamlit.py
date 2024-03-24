8feb2d643d9c355ab54c116d80544e246cdfdb11
Akshaya <Akshayap0504@gmail.com>
2024-03-24 21:58:41 +0000

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -68,7 +68,19 @@ with right_col:
 load_dotenv()
 
 # Get database credentials 
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 
+# set up db connection:
+
+def connect_to_db():
+    engine = create_engine(f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+    query = f'SELECT * FROM weather' 
+    data = pd.read_sql(query, engine)
+    return data
 
 
 
