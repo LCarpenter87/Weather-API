4b3e3d57625ca1b030191d18fee86a4b41611c7e
Akshaya <Akshayap0504@gmail.com>
2024-03-25 00:17:14 +0000

    Update test_connection.py

--- test_connection.py
+++ test_connection.py
@@ -1,4 +1,5 @@
 import os
+import streamlit as st
 
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
@@ -10,4 +11,16 @@ print("DB_NAME:", DB_NAME)
 print("DB_USER:", DB_USER)
 print("DB_PASSWORD:", DB_PASSWORD)
 print("DB_HOST:", DB_HOSTS)
+print("DB_PORT:", DB_PORT)
+
+DB_NAME = st.secrets["DB_NAME"]
+DB_USER = st.secrets["DB_USER"]
+DB_PASSWORD = st.secrets["DB_PASSWORD"]
+DB_HOSTS = st.secrets["DB_HOSTS"]
+DB_PORT = st.secrets["DB_PORT"]
+
+print("DB_NAME:", DB_NAME)
+print("DB_USER:", DB_USER)
+print("DB_PASSWORD:", DB_PASSWORD)
+print("DB_HOSTS:", DB_HOSTS)
 print("DB_PORT:", DB_PORT)
