c1eb7f081e4317f7906433d5f4bd442f176e4272
Akshaya <Akshayap0504@gmail.com>
2024-03-25 00:13:04 +0000

    Update test_connection.py

--- test_connection.py
+++ test_connection.py
@@ -6,4 +6,8 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOSTS = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 
-
+print("DB_NAME:", DB_NAME)
+print("DB_USER:", DB_USER)
+print("DB_PASSWORD:", DB_PASSWORD)
+print("DB_HOST:", DB_HOSTS)
+print("DB_PORT:", DB_PORT)
